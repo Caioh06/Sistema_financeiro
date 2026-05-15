eaf4917752d7a3761f49337766b36b634fe56e27
Caio Henrique M. Leite <caio.hm.leite2006@gmail.com>
2026-05-15 17:43:18 -0300

    Atualizando a versão offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'commitpay-v1';
+const CACHE_NAME = 'commitpay-v2'; 
 const ASSETS = [
     './',
     './index.html',
@@ -7,14 +7,41 @@ const ASSETS = [
     './manifest.json'
 ];
 
+
 self.addEventListener('install', (e) => {
+    self.skipWaiting(); 
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
+
+self.addEventListener('activate', (e) => {
+    self.clients.claim(); 
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
+        fetch(e.request)
+            .then((response) => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+                return response;
+            })
+            .catch(() => {
+                return caches.match(e.request);
+            })
     );
 });
